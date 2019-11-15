9a632b711332678986a97141e767fb3032a0389b
nazmul629 <nazmulhossain.qnh@gmail.com>
2019-11-15 16:36:40 +0600

    compleate some task, urls

--- src/book_shelf/urls.py
+++ src/book_shelf/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import index,profile,single_book
+from .views import index,profile,single_book,author_book,publication_book,category_book,book_details
 
 urlpatterns = [
-    path('',index),
+    path('',index,name='index'),
+    path('book/author/<book_auth>',author_book,name="author_book"),
+    path('book/pub/<book_pub>',publication_book,name="publisar_book"),
+    path('book/category/<book_cate>',category_book,name="category_book"),
+    path('book/details/<int:id>',book_details,name= "book_details"),
+
     path('profile',profile),
     path('single_book',single_book)
+
+
 ]
 
--- src/book_shelf/views.py
+++ src/book_shelf/views.py
@@ -19,6 +19,42 @@ def index(request):
     return render(request, 'index.html',context);
     
 
+def author_book(request,book_auth):
+    auth_books = NeedToRead.objects.filter(book_author__name = book_auth)
+    context = {
+        "auth_books":auth_books,
+        "book_autor_name":book_auth
+    }
+    return render(request,'book_shelf/author_book.html',context)
+
+
+
+def publication_book(request,book_pub):
+    pub_books = NeedToRead.objects.filter(publisher__name = book_pub)
+    context = {
+        "pubs_books" : pub_books,
+        "pub_name" : book_pub
+    }
+    return render(request,'book_shelf/pub_book.html',context)
+
+
+
+
+def category_book(request,book_cate):
+    cate_books = NeedToRead.objects.filter(category__name = book_cate)
+    context = {
+        "cate_books":cate_books,
+        'cate_name':book_cate
+    }
+    return render(request,'book_shelf/cate_book.html',context)
+
+def book_details(request,id):
+    books = NeedToRead.objects.get(id=id)
+    context = {
+        "book" : books
+    }
+    return render(request,'book_shelf/single.html',context)
+
 
 
 def profile(request):
@@ -26,3 +62,4 @@ def profile(request):
 
 def single_book(request):
     return render(request,'single.html');
+
